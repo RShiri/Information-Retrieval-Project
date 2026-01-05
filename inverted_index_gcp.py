6c81b67b8e740e10c07f2f434bbc702e87e147fc
RShiri <166461000+RShiri@users.noreply.github.com>
2026-01-06 00:25:48 +0200

    Update PROJECT_ID for GCP storage

--- inverted_index_gcp.py
+++ inverted_index_gcp.py
@@ -13,7 +13,7 @@ from google.cloud import storage
 from collections import defaultdict
 from contextlib import closing
 
-PROJECT_ID = 'storied-parser-482817-s7'
+PROJECT_ID = 'ir-project-2026'
 def get_bucket(bucket_name):
     return storage.Client(PROJECT_ID).bucket(bucket_name)
 
