4afdb2bd22625748400ed34a54fc2de44ca9b495
OrianRa <orianra@post.bgu.ac.il>
2026-01-10 13:25:47 +0200

    Update run_frontend_in_gcp.sh

--- run_frontend_in_gcp.sh
+++ run_frontend_in_gcp.sh
@@ -1,7 +1,7 @@
 INSTANCE_NAME="instance-1"
 REGION=us-central1
 ZONE=us-central1-c
-PROJECT_NAME="rising-dominion-480521-s2"
+PROJECT_NAME="Assignment 3"
 IP_NAME="$PROJECT_NAME-ip"
 GOOGLE_ACCOUNT_NAME="puzik7399" # without the @post.bgu.ac.il or @gmail.com part
 
